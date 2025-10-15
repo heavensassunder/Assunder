f0d83184949df215b947e01bc39e0b60cb4d04c1
heavensassunder <ndegwaroberts519@gmail.com>
2025-10-15 22:03:21 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -25,7 +25,7 @@ async function sendMessage() {
   renderChat();
 
   try {
-    const res = await fetch("http://127.0.0.1:5000/chat", {
+    const res = await fetch("https://assunder.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
